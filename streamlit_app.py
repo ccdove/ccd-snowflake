f5c53d1aa2907836e31f70190216f01630c23c1a
ccdove <141452897+ccdove@users.noreply.github.com>
2023-08-04 17:58:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('Healthy dinners count')
 streamlit.header('Big Breakfast')
 streamlit.text('Steak & 2 Eggs')
 streamlit.text('Ham mashed Cauliflower and redeye gravity')
-streamlit.text('Full English')
+streamlit.text('Full English') 
 
