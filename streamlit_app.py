9acc96757058ee136b199394c78084c417b0b074
ccdove <141452897+ccdove@users.noreply.github.com>
2023-08-04 18:08:59 -0400

    Update streamlit_app.py
    Add  S3 csv file and display it in a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('Healthy dinners count')
 streamlit.header('🥣 Big Breakfast')
@@ -7,3 +8,6 @@ streamlit.text('🐔 Ham mashed Cauliflower and redeye gravity')
 streamlit.text('🥑🍞 Full English') 
    
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
