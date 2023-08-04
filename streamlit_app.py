2316fbb5084779b07b5668ca1d97d348e859ab48
ccdove <141452897+ccdove@users.noreply.github.com>
2023-08-04 18:02:46 -0400

    Update streamlit_app.py
    add emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
 streamlit.title('Healthy dinners count')
-streamlit.header('Big Breakfast')
-streamlit.text('Steak & 2 Eggs')
-streamlit.text('Ham mashed Cauliflower and redeye gravity')
-streamlit.text('Full English') 
+streamlit.header('🥣 Big Breakfast')
+streamlit.text('🥗 Steak & 2 Eggs')
+streamlit.text('🐔 Ham mashed Cauliflower and redeye gravity')
+streamlit.text('🥑🍞 Full English') 
+   
 
