913260142c2b14564c1e46c316c6cbb9581aef1f
ccdove <141452897+ccdove@users.noreply.github.com>
2023-08-18 14:28:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,12 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/ + kiwi")
+#     streamlit.text(fruityvice_response.json())
+
+# flaytyens json? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# creates a dataframe of flattened data?
+streamlit.dataframe(fruityvice_normalized)
 
 
