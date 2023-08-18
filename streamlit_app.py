887e66f9bf30d810f56cb3b9b265038ff282998f
ccdove <141452897+ccdove@users.noreply.github.com>
2023-08-18 14:18:19 -0400

    Update streamlit_app.py
    add fruitybice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 import pandas
+import requests
+
 
 streamlit.title('Healthy dinners count')
 streamlit.header('🥣 Big Breakfast')
@@ -19,3 +21,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # display data from file
 #       streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
