9b04c2d20d0be65309b096c31a0333410eb8c6c6
ccdove <141452897+ccdove@users.noreply.github.com>
2023-09-08 17:27:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,21 +21,27 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), [ 'Avocado', 'Lime' ])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+
+
 # display data from file
 #       streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
+def: get_fruity_advice_data(this_fruit_choice)
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+         #     streamlit.text(fruityvice_response.json())
+         # flaytyens json? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
      streamlit.error('Please select a fruit to get information')
   else:
-    streamlit.header("Fruityvice Fruit Advice!")    
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-         #     streamlit.text(fruityvice_response.json())
-         # flaytyens json? 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-         # creates a dataframe of flattened data?
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.header("Fruityvice Fruit Advice!")   
+    back_from_function = get_fruity_advice_data(fruit_choice)
+             # creates a dataframe of flattened data?
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
