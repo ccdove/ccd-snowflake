92352a71fc3a652fafa0ec01c0d4769d13686ead
ccdove <141452897+ccdove@users.noreply.github.com>
2023-09-08 17:49:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), [ 'Avocado', 'Lime' ])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
+streamlit.dataframe(fruits_to_show)
 
 
 # display data from file
@@ -33,13 +33,12 @@ def get_fruity_advice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
-streamlit.dataframe(fruits_to_show)
+streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
      streamlit.error('Please select a fruit to get information')
   else:
-    streamlit.header("Fruityvice Fruit Advice!")   
     back_from_function = get_fruity_advice_data(fruit_choice)
              # creates a dataframe of flattened data?
     streamlit.dataframe(back_from_function)
